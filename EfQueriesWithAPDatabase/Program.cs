ff98c43d47d26ccc8b868e2edcd81b4907eb9cf3
arcum-omni <hut8dev@gmail.com>
2020-01-29 14:44:31 -0800

    Updated vendor query

--- EfQueriesWithAPDatabase/Program.cs
+++ EfQueriesWithAPDatabase/Program.cs
@@ -11,15 +11,19 @@ namespace EfQueriesWithAPDatabase
         static void Main(string[] args)
         {
             APContext db = new APContext();
+            // Log queries
+            // db.Database.Log = Console.Writeline;
 
             // All vendors in CA, using link query syntax
-            List<Vendor> caVendors = (from v in db.Vendors where v.VendorState == "CA" select v).ToList();
+            List<Vendor> caVendors = (from v in db.Vendors where v.VendorState == "CA" orderby v.VendorName select v).ToList();
 
             Console.WriteLine("Vendors in CA");
             foreach (Vendor currVendor in caVendors)
             {
                 Console.WriteLine(currVendor.VendorName);
             }
+            
+            Console.ReadKey();
         }
     }
 }
