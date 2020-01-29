d5ca19550fccb696b955b5617138ba77e3bfd1d6
arcum-omni <hut8dev@gmail.com>
2020-01-29 14:39:40 -0800

    Added vendor query

--- EfQueriesWithAPDatabase/Program.cs
+++ EfQueriesWithAPDatabase/Program.cs
@@ -10,6 +10,16 @@ namespace EfQueriesWithAPDatabase
     {
         static void Main(string[] args)
         {
+            APContext db = new APContext();
+
+            // All vendors in CA, using link query syntax
+            List<Vendor> caVendors = (from v in db.Vendors where v.VendorState == "CA" select v).ToList();
+
+            Console.WriteLine("Vendors in CA");
+            foreach (Vendor currVendor in caVendors)
+            {
+                Console.WriteLine(currVendor.VendorName);
+            }
         }
     }
 }
